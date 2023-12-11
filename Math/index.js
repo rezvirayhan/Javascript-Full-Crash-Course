5d8bf52b4fd38544d09899cfdb2aceadd1f309c4
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-11 14:12:31 +0600

    Javascript Math

---
+++ Math/index.js
@@ -0,0 +1,32 @@
+let a = 3.14;
+let b = 3.44;
+let c = 5.94;
+let d = 4.60;
+let e = 3.84;
+
+
+x = Math.random(a);
+x = Math.floor(b)
+x = Math.ceil(c)
+x = Math.pow(d, 2)
+x = Math.sqrt(e)
+
+
+console.log('Math Random', a);
+console.log('Math Floor', b);
+console.log('Math Ceil', c);
+console.log('Math Pow', d);
+console.log('Math Sqrt', e);
+
+// max Number and min Number 
+
+let ab = 31.14;
+let y = 5;
+let z = 9;
+let max;
+let min;
+
+maximum = Math.max(ab, y, z)
+minium = Math.min(ab, y, z)
+console.log('The Max Number is. ', maximum);
+console.log('The min Number is. ', minium);
