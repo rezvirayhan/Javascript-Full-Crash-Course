a67d527f50521eed2cbe25e22238675c88eb16ac
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-11 13:56:43 +0600

    Javascript Const

---
+++ Const/index.js
@@ -0,0 +1,10 @@
+// Const = a variable that can't be change;
+
+let pi = 3.1416;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter The Radius Of Circle");
+radius = Number(radius);
+circumference = 2 * pi * radius;
+console.log("The Circuference Is: ", circumference);
