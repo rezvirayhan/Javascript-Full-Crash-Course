38cf16d59c2563ad0b3fab96992086cc6ef03097
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-11 16:52:07 +0600

    Method Chaining

---
+++ Method Chaining/index.js
@@ -0,0 +1,7 @@
+/**
+ * Method Chaining = calling one method after another in one continuns line of code
+ */
+
+let userName = "Rezvi";
+let letter = userName.charAt(0).toUpperCase().trim();
+console.log(letter);
