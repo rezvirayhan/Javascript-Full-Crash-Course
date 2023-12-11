2ef6e3dc93d38a64591879f989a35be8087f7fe1
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-11 16:59:42 +0600

    If Statment

---
+++ If Statments/index.js
@@ -0,0 +1,26 @@
+/**
+ * If Statment = a basic from of disson making if a condation is true then do somthing if not then don't do it 
+ */
+
+let age = -2;
+
+if (age >= 18) {
+    console.log("You are an Adult");
+}
+else if (age < 0) {
+    console.log("You Haven't Been Born Yet");
+}
+else if (age >= 65) {
+    console.log("You Are a Seniro");
+}
+else {
+    console.log("You are a child");
+}
+
+let online = true;
+if (online) {
+    console.log("You Are Online");
+}
+else {
+    console.log("You Are Offline");
+}
