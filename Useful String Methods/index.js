271a6a159f0d3e6a5bd0e27e71f63ad9f93fce3a
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-11 15:32:24 +0600

    UserFul String Method

---
+++ Useful String Methods/index.js
@@ -0,0 +1,11 @@
+let userName = "Rezvi Rayhan";
+let phoneNumber = '01727-979966'
+// userName = userName.toLocaleLowerCase // All Lower Case
+// userName = userName.toUpperCase // All upper Case
+console.log("Length", userName.length);
+console.log("charAt", userName.charAt(0));
+console.log("indexOf", userName.indexOf("R"));
+console.log("lastIndexOf", userName.lastIndexOf("n"));
+
+phoneNumber = phoneNumber.replaceAll('-', '/')
+console.log(phoneNumber);
