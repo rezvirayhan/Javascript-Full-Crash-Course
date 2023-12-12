646b8ef36caf5bc2b93cb4f6937de8cc1e8ed5ed
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-12 13:13:04 +0600

    Nested Loops

--- Nested Loops/index.js
+++ Nested Loops/index.js
@@ -2,13 +2,13 @@
  * nested loop = a loop inside of another loop
  * 
  */
-
+let symbol = window.prompt("Enter a Simble")
 let rows = window.prompt("Enter Number Of Rows")
 let colums = window.prompt("Enter Number Of Colums")
 
 for (let i = 1; i <= rows; i += 1) {
     for (let j = 1; j <= colums; j += 1) {
-        document.getElementById('myLabel').innerHTML += j
+        document.getElementById('myLabel').innerHTML += symbol
     }
     document.getElementById("myLabel").innerHTML += "<br/>"
 }
