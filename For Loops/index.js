9aa8016902f3da286bae74b6b920a858e819ed42
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-12 12:20:26 +0600

    For  Loops

---
+++ For Loops/index.js
@@ -0,0 +1,22 @@
+/**
+ * For Loop = repate some code a 
+ * cetain amount of fimes.
+ * 
+ */
+
+
+// INCREMENT
+for (let counter = 1; counter <= 10; counter++) {
+    console.log(counter);
+}
+console.log("-------END--------");
+console.log("HAPPY NEW YEAR BY INCREMENT");
+
+// DECREMENT
+
+for (let i = 20; i > 0; i--) {
+    console.log("Decrement", i);
+}
+console.log("-------END--------");
+
+console.log("HAPPY NEW YEAR BY DECREMENT");
