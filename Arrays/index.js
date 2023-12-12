9efeb97f0a25da7864dd74928d67c5877b0c5eea
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-12 18:08:36 +0600

    Array Method

---
+++ Arrays/index.js
@@ -0,0 +1,14 @@
+/**
+ * Array = Think of it as   a variable
+ * that can store miltple value
+ */
+
+let fruits = ["Apple", "Orange", "Banana"]
+
+// fruits[0] = "Coconut"
+fruits.push("Coconut") // add an last element\
+fruits.pop() // remove last element
+fruits.unshift("Mango") // added first element
+fruits.shift() // remove first element
+
+console.log(fruits);
