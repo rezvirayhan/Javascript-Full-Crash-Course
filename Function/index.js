c556ad4512c7ebf2964e21d215a37212dbaf73da
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-12-12 13:19:55 +0600

    Function Birthday Song

---
+++ Function/index.js
@@ -0,0 +1,19 @@
+/**
+ * Function = Define code once and use it many times 
+ * to prefrom some code call the function name. 
+ */
+
+let userName = "Rezvi"
+let age = 21
+
+function happyBirthday() {
+    console.log("Happy Birthday To You.");
+    console.log("Happy Birthday To You.");
+    console.log("Happy Birthday To Dear", userName);
+    console.log("Happy Birthday To You.");
+    console.log("You Are ", age, "Years Old");
+}
+
+happyBirthday()
+happyBirthday()
+happyBirthday()
